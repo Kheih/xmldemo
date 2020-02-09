4fbcaeed49efdc58aafb3f8ba969c41ff22c9f59
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-09 14:14:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://xmlactivity.herokuapp.com/rss.php");//XML page URL
  
- $content = $domOBJ->getElementsByTagName("articles");
+ $content = $domOBJ->getElementsByTagName("music");
  ?>
 
  <h1>Articles</h1>
@@ -10,16 +10,16 @@
  <?php
  foreach( $content as $data )
  {
-     $title = $data->getElementsByTagName("article_title")->item(0)->nodeValue;
-     $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
-     $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
-     $created = $data->getElementsByTagName("created")->item(0)->nodeValue;
+     $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+     $description = $data->getElementsByTagName("artist")->item(0)->nodeValue;
+     $author = $data->getElementsByTagName("genre")->item(0)->nodeValue;
+     $created = $data->getElementsByTagName("writer")->item(0)->nodeValue;
      echo "<ul>
             <h2>$title</h2>
               <ul>
-                  <li>Description: $description </li>
-                  <li>Author: $author </li>
-                  <li>Date Created: $created </li>
+                  <li>Artist: $artist </li>
+                  <li>Genre: $genre </li>
+                  <li>Writer: $writer </li>
               </ul>
           </ul>";
  }
