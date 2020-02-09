5c895a3032ecaca13e2bd858a4b68844bb14a3fb
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-09 13:53:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
-<!DOCTYPE html>
-<html>
-<body onload="loadXMLDoc()">
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xmlactivity.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("articles");
+ ?>
 
-<h2>Using the XMLHttpRequest Object</h2>
+ <h1>Articles</h1>
 
-<div id="demo">
-</div>
-
-<script>
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "emails.xml", true);
-  xhttp.send();
-}
-</script>
-</body>
-</html>
+ <?php
+ foreach( $content as $data )
+ {
+     $title = $data->getElementsByTagName("article_title")->item(0)->nodeValue;
+     $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+     $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+     $created = $data->getElementsByTagName("created")->item(0)->nodeValue;
+     echo "<ul>
+            <h2>$title</h2>
+              <ul>
+                  <li>Description: $description </li>
+                  <li>Author: $author </li>
+                  <li>Date Created: $created </li>
+              </ul>
+          </ul>";
+ }
+?>
