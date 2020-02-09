00d3f853e31e12f796911efa29aa25351ee41758
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-09 14:09:42 +0800

    Update rss-cnn.php

--- rss-cnn.php
+++ rss-cnn.php
@@ -1,22 +1,24 @@
 <?php
- $domOBJ = new DOMDocument();
- $domOBJ->load("http://rss.cnn.com/rss/edition.rss");//XML page URL
- 
- $content = $domOBJ->getElementsByTagName("item");
- 
- ?>
- <ul>
-    <?php
- foreach( $content as $data )
- {
-   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
-  
-   echo "<li>$title
-            <ul>
-                <li>$link</li>
-            </ul>
-        </li>";
- }
+    $rssf= '<?xml version="1.0" encoding="UTF-8"?>';
+    $rssf .= '<rss version="2.0">';
+    $rssf .= '<channel>';
+
+    $con = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820410") or die (mysqli_error($con));
+    $sql = "SELECT * FROM tblmusic;";
+    $q = mysqli_query($con, $sql) or die (mysqli_error($con));
+
+    while($row= mysqli_fetch_assoc($query)){
+        extract($row);
+        
+        $rssf .= '<music>';
+        $rssf .= '<title>' . $mus_title . '</title>';
+        $rssf .= '<artist>' . $mus_artist . '</artist>';
+        $rssf .= '<genre>' . $mus_genre . '</genre>';
+        $rssf .= '<writer>' . $mus_writer . '</writer>';
+        $rssf .= '</music>';
+    }
+    $rssf .= '</channel>';
+    $rssf .= '</rss>';
+
+    echo $rssf;
 ?>
-</ul>
