1da5cc71a341fb25fb9927ce3f2e631e9e25206b
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-09 14:34:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,18 @@
  $domOBJ->load("https://newapp-faus.herokuapp.com/rssf.php");//XML page URL
  
  $content = $domOBJ->getElementsByTagName("music");
- ?>
+?>
 
- <h1>Music</h1>
+ <h1>Top 5 Music in 2020</h1>
 
- <?php
- foreach( $content as $data )
- {
+<?php
+ foreach( $content as $data ){
      $mus_title = $data->getElementsByTagName("mus_title")->item(0)->nodeValue;
      $mus_artist = $data->getElementsByTagName("mus_artist")->item(0)->nodeValue;
      $mus_genre = $data->getElementsByTagName("mus_genre")->item(0)->nodeValue;
      $mus_writer = $data->getElementsByTagName("mus_writer")->item(0)->nodeValue;
      echo "<ul>
-            <h2>$title</h2>
+            <h2>$mus_title</h2>
               <ul>
                   <li>Artist: $mus_artist </li>
                   <li>Genre: $mus_genre </li>
